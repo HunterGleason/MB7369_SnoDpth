d8508371d9aa3e97b5c0ffe850e9051589dc04ae
HunterGleason <Hunter.Gleason@gov.bc.ca>
2021-10-20 16:42:06 -0700

    Changed from sending snapshot measurements to daily min and max over Iridium, sent at midnight, global constant vars now read from paramter file (snowlog.csv) off SD card

--- MB7369_SnoDpth.ino
+++ MB7369_SnoDpth.ino
@@ -14,6 +14,7 @@
 #include "Adafruit_SHT31.h"//Needed for SHT30 Temp/Humid sensor
 #include <IridiumSBD.h>
 #include <Wire.h>
+#include <CSV_Parser.h>//Needed for parsing CSV data 
 
 /*Create library instances*/
 RTC_PCF8523 rtc; //instantiate PCF8523 RTC
@@ -33,11 +34,12 @@ const byte donePin = 10; //Done pin for TPL5110 power breakout
 const byte chipSelect = 4; //Chip select pin for MicroSD breakout
 const byte led = 13; // Pin 13 LED
 
-/*Global constants (!!General user modify code in this section only!!)*/
-const String filename = "SnoDTEST.TXT";//Desired name for data file !!!must be less than equal to 8 char!!!
-const long N = 6; //Number of sensor readings to average.
-const int transmitHours[] = {0};//List of hours for which daily Iridium transmission is to take place (0-23). 
-const int logging_intv_min = 5;//Should match settings on TPL5110 (i.e., the switch combination), when using Iridium recommended min logging interval of >=5-min do to required transmit time during periods of poor signal quality, otherwise tranmission may fail. 
+
+
+/*Global constants*/
+char **filename;//Desired name for data file !!!must be less than equal to 8 char!!!
+char **N; //Number of ultrasonic reange sensor readings to average.
+char **logging_intv_min;//Should match settings on TPL5110 (i.e., the switch combination), when using Iridium recommended min logging interval of >=5-min do to required transmit time during periods of poor signal quality, otherwise tranmission may fail. 
 
 /*Global variables*/
 long distance; //Variable for holding distance read from MaxBotix MB7369 ultrasonic ranger
@@ -71,33 +73,6 @@ long read_sensor(int N) {
   return avg_dist;
 }
 
-//Function loops through Iridium transmit hours (0-23) specified in transmitHours and checks if the current hour matches, and the minute is less then or equal to one logging interval (logging_intv_min)
-boolean check_transmit()
-{
-  boolean transmit = false;//Assume false for transmit
-
-  DateTime now = rtc.now();//Get the current time from RTC, including hour and minute values
-  int current_hour = now.hour();
-  int current_minute = now.minute();
-
-  int ary_size = sizeof(transmitHours);//Get size of transmitHours array
-
-  //For each transmit hour in transmitHours
-  for (int i = 0; i < (sizeof(transmitHours) / sizeof(transmitHours[0])); i++)
-  {
-    //Check if it matches the current hour, and minute is less then or equal to 1 logging interval, dictated by the TPL5110 settings, and specified by logging_intv_min
-    if (transmitHours[i] == current_hour && current_minute <= logging_intv_min)
-    {
-      //If the current time is a transmit period set transmit to true
-      transmit = true;
-    }
-
-    delay(1);
-  }
-
-  return transmit;
-
-}
 
 //Code runs once upon waking up the TPL5110
 void setup() {
@@ -133,6 +108,26 @@ void setup() {
   modem.enable841lowPower(true); // Enable the ATtiny841's low power mode
 
 
+  //Make sure a SD is available (1/2-sec flash LED means SD card did not initialize)
+  while (!SD.begin(chipSelect))
+  {
+    digitalWrite(led, HIGH);
+    delay(500);
+    digitalWrite(led, LOW);
+    delay(500);
+  }
+
+  //Set paramters for parsing the parameter file
+  CSV_Parser cp(/*format*/ "sss", /*has_header*/ true, /*delimiter*/ ',');
+
+  //Read the parameter file off SD card (snowlog.csv), see README.md 
+  cp.readSDfile("/snowlog.csv");
+
+  //Read values from SNOW_PARAM.TXT into global varibles 
+  filename = (char**)cp["filename"];
+  N = (char**)cp["N"];
+  logging_intv_min = (char**)cp["log_intv"];
+
   // Start RTC (1-sec flash LED means RTC did not initialize)
   while (!rtc.begin()) {
     digitalWrite(led, HIGH);
@@ -145,7 +140,7 @@ void setup() {
   DateTime now = rtc.now();
 
   //Read N average ranging distance from MB7369
-  distance = read_sensor(N);
+  distance = read_sensor(String(N[0]).toInt());
 
 
   while (!sht31.begin(0x44)) {  // Start SHT30, Set to 0x45 for alternate i2c addr (2-sec flash LED means SHT30 did not initialize)
@@ -168,35 +163,162 @@ void setup() {
     sht31.heater(false);
   }
 
-
-  //Assemble a data string for logging to SD, with date-time, snow depth (mm), temperature (deg C) and humidity (%)
-  String datastring = String(now.year()) + "-" + String(now.month()) + "-" + String(now.day()) + " " + String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second()) + "," + String(distance) + ",mm," + String(temp_c) + ",deg C," + String(humid_prct) + ",%";
-
-  //Make sure a SD is available (1/2-sec flash LED means SD card did not initialize)
-  while (!SD.begin(chipSelect)) {
-    digitalWrite(led, HIGH);
-    delay(500);
-    digitalWrite(led, LOW);
-    delay(500);
+  //Format current date time values for writing to SD
+  String yr_str = String(now.year());
+  String mnth_str;
+  if (now.month() >= 10)
+  {
+    mnth_str = String(now.month());
+  } else {
+    mnth_str = "0" + String(now.month());
   }
 
+  String day_str;
+  if (now.day() >= 10)
+  {
+    day_str = String(now.day());
+  } else {
+    day_str = "0" + String(now.day());
+  }
+
+  String hr_str;
+  if (now.hour() >= 10)
+  {
+    hr_str = String(now.hour());
+  } else {
+    hr_str = "0" + String(now.hour());
+  }
+
+  String min_str;
+  if (now.minute() >= 10)
+  {
+    min_str = String(now.minute());
+  } else {
+    min_str = "0" + String(now.minute());
+  }
+
+
+  String sec_str;
+  if (now.second() >= 10)
+  {
+    sec_str = String(now.second());
+  } else {
+    sec_str = "0" + String(now.second());
+  }
+
+  //Assemble a data string for logging to SD, with date-time, snow depth (mm), temperature (deg C) and humidity (%)
+  String datastring = yr_str + "-" + mnth_str + "-" + day_str + " " + hr_str + ":" + min_str + ":" + sec_str + "," + String(distance) + ",mm," + String(temp_c) + ",deg C," + String(humid_prct) + ",%";
+
   //Write datastring and close logfile on SD card
-  dataFile = SD.open(filename, FILE_WRITE);
+  dataFile = SD.open(String(filename[0]), FILE_WRITE);
   if (dataFile)
   {
     dataFile.println(datastring);
     dataFile.close();
   }
 
-  //Check that the iridium modem is connected and the hour matches one of the desired transmit hours (0-24) (transmitHours[])
-  if (check_transmit() && modem.isConnected())
+  //Check that the iridium modem is connected and the the clock has just reached midnight (i.e.,current time is within one logging interval of midnight)
+  if (now.hour() == 0 && now.minute() <= String(logging_intv_min[0]).toInt() && modem.isConnected())
   {
-    //digitalWrite(led, HIGH); //For trouble shooting
+   
 
     modem.enableSuperCapCharger(true); // Enable the super capacitor charger
     while (!modem.checkSuperCapCharger()) ; // Wait for the capacitors to charge
     modem.enable9603Npower(true); // Enable power for the 9603N
     modem.begin(); // Wake up the 9603N and prepare it for communications.
+
+    /*Need to obtain the daily observations from the SD card, caclulate daily min / max for snow depth, temperture and RH, and send results over Iridium*/ 
+    
+    //Get the datetime of the days start (i.e., 24 hours previous to current time)
+    DateTime days_start (now - TimeSpan(1,0,0,0));
+
+    //Set paramters for parsing the log file
+    CSV_Parser cp("ss-s-s-", false, ',');
+
+    //Varibles for holding data fields 
+    char **datetimes;
+    long *snowdepths;
+    float *temps;
+    float *rh;
+
+    //Parse the logfile
+    cp.readSDfile(filename[0]);
+
+    //Populate data arrays from logfile
+    datetimes = (char**)cp[0];
+    snowdepths = (long*)cp[1];
+    temps = (float*)cp[3];
+    rh = (float*)cp[5];
+
+    //Daily min / max varibles, set to extreams 
+    float min_depth=6000;
+    float max_depth=-1;
+    float min_temp=100;
+    float max_temp=-100;
+    float min_rh=100;
+    float max_rh=-1;
+
+    //For each observation in the CSV 
+    for (int i = 0; i < cp.getRowsCount(); i++) {
+
+      //Get the datetime stamp as string 
+      String datetime = String(datetimes[i]);
+
+      //Get the observations year, month, day
+      int dt_year = datetime.substring(0, 4).toInt();
+      int dt_month = datetime.substring(5, 7).toInt();
+      int dt_day = datetime.substring(8, 10).toInt();
+
+      //Check if the observations datetime occured during the day being summarised  
+      if (dt_year == days_start.year() && dt_month == days_start.month() && dt_day == days_start.day())
+      {
+        //Update min snow depth
+        if(snowdepths[i]<min_depth)
+        {
+          min_depth=snowdepths[i];
+        }
+        //Update max snow depth
+        if(snowdepths[i]>max_depth)
+        {
+          max_depth=snowdepths[i];
+        }
+        //Update min temp
+        if(temps[i]<min_temp)
+        {
+          min_temp=temps[i];
+        }
+        //Update max temp
+        if(temps[i]>max_temp)
+        {
+          max_temp=temps[i];
+        }
+        //Update min rh
+        if(rh[i]<min_rh)
+        {
+          min_rh=rh[i];
+        }
+        //Update max rh
+        if(rh[i]>max_rh)
+        {
+          max_rh=rh[i];
+        }
+
+      }
+
+    }
+
+    //A data string with daily min / max results for sending over Iridium 
+    datastring = "{" + yr_str + "-" + mnth_str + "-" + day_str + " " + hr_str + ":" + min_str + ":" + sec_str + min_depth + "," + max_depth + "," + min_temp + "," + max_temp + "," + min_rh + "," + max_rh + "}";   
+
+    //Delete this block, trouble shooting 
+    dataFile = SD.open(String(filename[0]), FILE_WRITE);
+    if (dataFile)
+    {
+      dataFile.println(datastring);
+      dataFile.close();
+    }
+
+    
     modem.sendSBDText(datastring.c_str()); // Send a message
     modem.sleep(); // Put the modem to sleep
     modem.enable9603Npower(false); // Disable power for the 9603N
