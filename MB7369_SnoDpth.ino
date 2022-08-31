5e215801ebd63dd3e26a8399dd5a01fd5e665aa8
Gleason <Hunter.Gleason@gov.bc.ca>
2022-08-31 14:17:16 -0700

    edited Iridium function so that IRID.CSV and HOURLY.CSV are deleted / updated before trying to send message, in event the TPL kills power before transmission

--- MB7369_SnoDpth.ino
+++ MB7369_SnoDpth.ino
@@ -14,7 +14,7 @@ File dataFile;//Instantiate a logging file
 Adafruit_SHT31 sht31 = Adafruit_SHT31();//Instantiate SHT30 sensor
 QuickStats stats; //Instantiate QuickStats
 #define IridiumWire Wire//Instantiate Wire 
-IridiumSBD modem(IridiumWire);//Instantiate IridiumSBD as modem 
+IridiumSBD modem(IridiumWire);//Instantiate IridiumSBD as modem
 
 
 /*Define pinouts*/
@@ -30,8 +30,8 @@ int16_t *N; //Number of ultrasonic range sensor readings to compute median.
 int16_t *ultrasonic_height_mm; //Reqiired for comuting snow depth, ignored if configured as stage_mm
 int16_t *irid_freq; // Iridium transmit freqency in hours
 char **start_time;// Time at which first Iridum transmission should occur
-char **irid_time;// For keeping track of IRIDIUM transmit times 
-char **metrics_letter_code;// Code for adding correct letter code to Iridium string, e.g., 'AFG', (see 'metrics' table in database for letter codes) 
+char **irid_time;// For keeping track of IRIDIUM transmit times
+char **metrics_letter_code;// Code for adding correct letter code to Iridium string, e.g., 'AFG', (see 'metrics' table in database for letter codes)
 String metrics;// String representation of metrics_letter_code
 boolean report_raw_dist = true;// Boolean indicating if raw distance (stage) or depth (snow) should be logged
 
@@ -40,11 +40,12 @@ int16_t distance; //Variable for holding distance read from MaxBotix MB7369 ultr
 int16_t duration; //Variable for holding pw duration read from MaxBotix MB7369 ultrasonic ranger
 float temp_deg_c; //Variable for holding SHT30 temperature
 float rh_prct; //Variable for holding SHT30 humidity
+DateTime irid_time_; //Varible for holding Iridium transmit time
 
 /*Define User Functions*/
 int16_t read_sensor(int N)  //Function for sampling MaxBotix ultrasonic ranger, returns N median distance in mm
 {
-  float values[N];//Array for holding sampled distances 
+  float values[N];//Array for holding sampled distances
   digitalWrite(triggerPin, HIGH);//Drive ranging pin HIGH
   delay(1000);//Let instrument settle
 
@@ -58,9 +59,9 @@ int16_t read_sensor(int N)  //Function for sampling MaxBotix ultrasonic ranger, 
 
   digitalWrite(triggerPin, LOW);//Terminate ranging
 
-  int16_t med_distance = round(stats.median(values, N));//Compute median 
+  int16_t med_distance = round(stats.median(values, N));//Compute median
 
-  return med_distance;//Return median 
+  return med_distance;//Return median
 }
 
 int send_hourly_data() //Function reads contents of HOURLY.CSV and sends hourly average data values over IRIDIUM formatted to be digested by database CGI script
@@ -73,7 +74,7 @@ int send_hourly_data() //Function reads contents of HOURLY.CSV and sends hourly 
   modem.enableSuperCapCharger(true); // Enable the super capacitor charger
 
   err = modem.begin();// Begin satellite modem operation, blink led twice for (1-sec) if there was an issue
-  
+
   if (err != ISBD_SUCCESS)
   {
     digitalWrite(led, HIGH);
@@ -89,7 +90,7 @@ int send_hourly_data() //Function reads contents of HOURLY.CSV and sends hourly 
   CSV_Parser cp("sdff", true, ',');  // Set paramters for parsing the log file
 
   char **datetimes; //pointer for datetimes
-  int16_t *distances;//pointer for distances 
+  int16_t *distances;//pointer for distances
   float *air_temps;//pointer for air temps
   float *rhs;//pointer for RHs
 
@@ -105,7 +106,7 @@ int send_hourly_data() //Function reads contents of HOURLY.CSV and sends hourly 
     distances = (int16_t*)cp["snow_depth_mm"];//populate snow
   }
   air_temps = (float*)cp["air_temp_deg_c"];//populate air temps
-  rhs = (float*)cp["rh_prct"];//populate RHs 
+  rhs = (float*)cp["rh_prct"];//populate RHs
 
   String datastring = metrics + ":" + String(datetimes[0]).substring(0, 10) + ":" + String(datetimes[0]).substring(11, 13) + ":";  //Formatted for CGI script >> sensor_letter_code:date_of_first_obs:hour_of_first_obs:data
 
@@ -126,15 +127,15 @@ int send_hourly_data() //Function reads contents of HOURLY.CSV and sends hourly 
   {
     intvl_dt = start_dt + TimeSpan(0, 1, 0, 0);//Set the intvl_dt as start_dt + 1 hour
 
-    float mean_depth;//var for mean depth / distance 
-    float mean_temp;//var for mean air temp 
+    float mean_depth;//var for mean depth / distance
+    float mean_temp;//var for mean air temp
     float mean_rh;//var for mean air RH
     boolean is_first_obs = true;//boolean for indicating first observation in HOURLY.CSV
     int N = 0;//var for # of obs
 
     for (int i = 0; i < num_rows; i++) {//For each observation in the HOURLY.CSV
 
-      String datetime = String(datetimes[i]);//get the datestamp at row and parse datetime 
+      String datetime = String(datetimes[i]);//get the datestamp at row and parse datetime
       int dt_year = datetime.substring(0, 4).toInt();
       int dt_month = datetime.substring(5, 7).toInt();
       int dt_day = datetime.substring(8, 10).toInt();
@@ -142,14 +143,14 @@ int send_hourly_data() //Function reads contents of HOURLY.CSV and sends hourly 
       int dt_min = datetime.substring(14, 16).toInt();
       int dt_sec = datetime.substring(17, 19).toInt();
 
-      DateTime obs_dt = DateTime(dt_year, dt_month, dt_day, dt_hour, dt_min, dt_sec);//Create a date time from datestamp at row i 
+      DateTime obs_dt = DateTime(dt_year, dt_month, dt_day, dt_hour, dt_min, dt_sec);//Create a date time from datestamp at row i
 
-      if (obs_dt >= start_dt && obs_dt <= intvl_dt)//If the observations is within the current hourly interval 
+      if (obs_dt >= start_dt && obs_dt <= intvl_dt)//If the observations is within the current hourly interval
       {
 
-        float snow_depth = (float) distances[i];//snow depth (or stage) is distances at i 
+        float snow_depth = (float) distances[i];//snow depth (or stage) is distances at i
         float air_temp = air_temps[i];// air temp is air_temps at i
-        float rh = rhs[i];// air RH is rhs at i 
+        float rh = rhs[i];// air RH is rhs at i
 
 
         if (is_first_obs == true)//Check if this is the first observation for the hour
@@ -158,13 +159,13 @@ int send_hourly_data() //Function reads contents of HOURLY.CSV and sends hourly 
           mean_depth = snow_depth;//set avg equal to first obs
           mean_temp = air_temp;
           mean_rh = rh;
-          is_first_obs = false;//update state of is_first_obs to false 
-          N++;//increment obs count 
+          is_first_obs = false;//update state of is_first_obs to false
+          N++;//increment obs count
         } else {
-          mean_depth = mean_depth + snow_depth;//add value at i to mean depth sum 
+          mean_depth = mean_depth + snow_depth;//add value at i to mean depth sum
           mean_temp = mean_temp + air_temp;
           mean_rh = mean_rh + rh;
-          N++;//increment obs count 
+          N++;//increment obs count
         }
 
       }
@@ -173,14 +174,14 @@ int send_hourly_data() //Function reads contents of HOURLY.CSV and sends hourly 
     if (N > 0)//Check if there were any observations for the hour
     {
       mean_depth = mean_depth / N;//compute mean depth (distance)
-      mean_temp = (mean_temp / N) * 10.0;//compute mean temp, convert to integer 
-      mean_rh = (mean_rh / N) * 10.0;//compute mean RH, convert to integer 
+      mean_temp = (mean_temp / N) * 10.0;//compute mean temp, convert to integer
+      mean_rh = (mean_rh / N) * 10.0;//compute mean RH, convert to integer
 
       datastring = datastring + String(round(mean_depth)) + ',' + String(round(mean_temp)) + ',' + String(round(mean_rh)) + ':';//Assemble the data string
 
     }
 
-    start_dt = intvl_dt;//update start_dt to equal intvl_dt,i.e.,next hour start 
+    start_dt = intvl_dt;//update start_dt to equal intvl_dt,i.e.,next hour start
   }
 
   uint8_t dt_buffer[340];//Binary bufffer for iridium transmission (max allowed buffer size 340 bytes)
@@ -196,25 +197,37 @@ int send_hourly_data() //Function reads contents of HOURLY.CSV and sends hourly 
     buffer_idx++;//increment the buffer index
   }
 
+  SD.remove("/HOURLY.CSV");//Remove previous HOURLY.CSV so that only new values will be sent next transmit
+
+  irid_time_ =  irid_time_ + TimeSpan(0, irid_freq[0], 0, 0);//Increment the Iridium transmit time by the Iridium transmit freq.
+
+  SD.remove("IRID.CSV");//Remove the old IRID.CSV file
+
+  dataFile = SD.open("IRID.CSV", FILE_WRITE);//Create new IRID.CSV file
+  if (dataFile)
+  {
+    dataFile.println("irid_time");//write header
+    dataFile.print(irid_time_.timestamp());//write iridium transmit time
+    dataFile.close();//close file
+  }
+
   digitalWrite(led, HIGH);//Indicate the modem is trying to send with led
 
   err = modem.sendSBDBinary(dt_buffer, buffer_idx);//transmit binary buffer data via iridium
 
-  if (err != 0 && err != 13)// If first attemped failed try once more with increased timeout 
+  if (err != 0 && err != 13)// If first attemped failed try once more with increased timeout
   {
     err = modem.begin();
     modem.adjustSendReceiveTimeout(500);
     err = modem.sendSBDBinary(dt_buffer, buffer_idx);
   }
-  digitalWrite(led, LOW);//indicate no longer trying to send 
-
-  SD.remove("/HOURLY.CSV");//Remove previous HOURLY.CSV so that only new values will be sent next transmit
+  digitalWrite(led, LOW);//indicate no longer trying to send
 
   return err;//Return err code
 
 }
 
-void setup() {//Code runs once upon waking up of the TPL5110 power timer 
+void setup() {//Code runs once upon waking up of the TPL5110 power timer
 
   Wire.begin();// Start the I2C wire port connected to the satellite modem
 
@@ -233,10 +246,10 @@ void setup() {//Code runs once upon waking up of the TPL5110 power timer
 
   pinMode(led, OUTPUT);//Set led pin as output
   digitalWrite(led, LOW);//drive led low
-  pinMode(pulsePin, INPUT);//set pulse width pin as input 
+  pinMode(pulsePin, INPUT);//set pulse width pin as input
   pinMode(triggerPin, OUTPUT);//set ranging trigger pin as output
   digitalWrite(triggerPin, LOW);//drive ranging trigger pin HIGH
-  pinMode(donePin, OUTPUT);//set done pin as outout 
+  pinMode(donePin, OUTPUT);//set done pin as outout
 
   while (!rtc.begin()) {// Start RTC (1-sec flash led means RTC did not initialize)
     digitalWrite(led, HIGH);
@@ -252,25 +265,25 @@ void setup() {//Code runs once upon waking up of the TPL5110 power timer
     delay(500);
   }
 
-  CSV_Parser cp(/*format*/ "sddsds", /*has_header*/ true, /*delimiter*/ ',');//Set parameters for parsing the parameter file 
+  CSV_Parser cp(/*format*/ "sddsds", /*has_header*/ true, /*delimiter*/ ',');//Set parameters for parsing the parameter file
 
-  cp.readSDfile("/PARAM.txt");//Read the PARAM.txt file 
+  cp.readSDfile("/PARAM.txt");//Read the PARAM.txt file
 
   filename = (char**)cp["filename"];//Get the filename parameter
   N = (int16_t*)cp["N"];//Get the sample N parameter
   irid_freq = (int16_t*)cp["irid_freq"];//Get the iridium frequency parameter
-  start_time = (char**)cp["start_time"];//Get the start time parameter 
+  start_time = (char**)cp["start_time"];//Get the start time parameter
   ultrasonic_height_mm = (int16_t*)cp["ultrasonic_height_mm"];//Get the ultrasonic ranger height parameter
-  metrics_letter_code = (char**)cp["metrics_letter_code"];//Get the metrics letter code parameter 
+  metrics_letter_code = (char**)cp["metrics_letter_code"];//Get the metrics letter code parameter
 
-  metrics = String(metrics_letter_code[0]);//Get metrics letter code as Arduino String 
+  metrics = String(metrics_letter_code[0]);//Get metrics letter code as Arduino String
 
-  if (metrics.charAt(0) == 'E')//Check if first letter in letter code is 'E', i.e., snow depth (see database) 
+  if (metrics.charAt(0) == 'E')//Check if first letter in letter code is 'E', i.e., snow depth (see database)
   {
-    report_raw_dist = false;//If snow depth, do not report raw distance value,set false 
+    report_raw_dist = false;//If snow depth, do not report raw distance value,set false
   }
 
-  String filestr = String(filename[0]);//get name of logfile 
+  String filestr = String(filename[0]);//get name of logfile
 
   DateTime dt = rtc.now();//Get datetime from RTC
 
@@ -279,9 +292,9 @@ void setup() {//Code runs once upon waking up of the TPL5110 power timer
     dataFile = SD.open("IRID.CSV", FILE_WRITE);//Open a IRID.CSV file for writing
     if (dataFile)
     {
-      dataFile.println("irid_time");//write header 
-      dataFile.print(dt.timestamp(DateTime::TIMESTAMP_DATE) + "T" + start_time[0]);//write the next transmit time 
-      dataFile.close();//close the file 
+      dataFile.println("irid_time");//write header
+      dataFile.print(dt.timestamp(DateTime::TIMESTAMP_DATE) + "T" + start_time[0]);//write the next transmit time
+      dataFile.close();//close the file
     }
   }
 
@@ -297,30 +310,18 @@ void setup() {//Code runs once upon waking up of the TPL5110 power timer
   int irid_min = String(irid_time[0]).substring(14, 16).toInt();
   int irid_sec = String(irid_time[0]).substring(17, 19).toInt();
 
-  DateTime irid_time_ = DateTime(irid_year, irid_month, irid_day, irid_hr, irid_min, irid_sec);//Construct Iridium transmit time
+  irid_time_ = DateTime(irid_year, irid_month, irid_day, irid_hr, irid_min, irid_sec);//Construct Iridium transmit time
 
   if (dt >= irid_time_)//Check if the current time is greater then or equal to the iridium transmit time
   {
-    send_hourly_data();//Send the hourly averages over Iridium 
-    irid_time_ =  irid_time_ + TimeSpan(0, irid_freq[0], 0, 0);//Increment the Iridium transmit time by the Iridium transmit freq. 
-
-    SD.remove("IRID.CSV");//Remove the old IRID.CSV file 
-
-    dataFile = SD.open("IRID.CSV", FILE_WRITE);//Create new IRID.CSV file 
-    if (dataFile)
-    {
-      dataFile.println("irid_time");//write header 
-      dataFile.print(irid_time_.timestamp());//write iridium transmit time 
-      dataFile.close();//close file 
-    }
-
+    send_hourly_data();//Send the hourly averages over Iridium
   }
 
-  distance = read_sensor(N[0]);//Read N average ranging distance from MaxBotix ultrasonic ranger 
+  distance = read_sensor(N[0]);//Read N average ranging distance from MaxBotix ultrasonic ranger
 
   if (report_raw_dist == false)//if not reporting raw distance (stage), report depth(snow)
   {
-    distance = ultrasonic_height_mm[0] - distance;//Compute depth as height of instrument - distance 
+    distance = ultrasonic_height_mm[0] - distance;//Compute depth as height of instrument - distance
   }
 
 
@@ -334,15 +335,15 @@ void setup() {//Code runs once upon waking up of the TPL5110 power timer
   temp_deg_c = sht31.readTemperature();//Get temp. reading from SHT30
   rh_prct = sht31.readHumidity();//Get RH reading from SHT30
 
-  float t_dew = temp_deg_c -  ((100.0-rh_prct)/5.0);//Approx. T-dew
+  float t_dew = temp_deg_c -  ((100.0 - rh_prct) / 5.0); //Approx. T-dew
 
   if (temp_deg_c <= t_dew )//If condensation, activate heater and retake humidity measurement
   {
-    sht31.heater(true);//Turn on SHT30 heater 
+    sht31.heater(true);//Turn on SHT30 heater
     delay(5000);//Give some time for heater to warm up
     sht31.heater(false);//Turn of heater
     delay(1000);
-    rh_prct = sht31.readHumidity();//Retake RH sample  
+    rh_prct = sht31.readHumidity();//Retake RH sample
   }
 
   String datastring = dt.timestamp() + "," + distance + "," + temp_deg_c + "," + rh_prct;//construct data string
@@ -354,25 +355,25 @@ void setup() {//Code runs once upon waking up of the TPL5110 power timer
     {
       if (report_raw_dist == false)//if snow
       {
-        dataFile.println("datetime,snow_depth_mm,air_temp_deg_c,rh_prct");//print snow header 
-      } else {//if stage 
-        dataFile.println("datetime,stage_mm,air_temp_deg_c,rh_prct");//print stage header 
+        dataFile.println("datetime,snow_depth_mm,air_temp_deg_c,rh_prct");//print snow header
+      } else {//if stage
+        dataFile.println("datetime,stage_mm,air_temp_deg_c,rh_prct");//print stage header
       }
-      dataFile.close();//close log file 
+      dataFile.close();//close log file
     }
 
   } else {//Write datastring and close logfile on SD card
-   
-    dataFile = SD.open(filestr.c_str(), FILE_WRITE);//open log file 
+
+    dataFile = SD.open(filestr.c_str(), FILE_WRITE);//open log file
     if (dataFile)
     {
-      dataFile.println(datastring);//print data string 
-      dataFile.close();//close log file 
+      dataFile.println(datastring);//print data string
+      dataFile.close();//close log file
     }
 
   }
 
-  if (!SD.exists("HOURLY.CSV"))//HOURLY.CSV duplicates the logfile 
+  if (!SD.exists("HOURLY.CSV"))//HOURLY.CSV duplicates the logfile
   {
     dataFile = SD.open("HOURLY.CSV", FILE_WRITE);
     if (dataFile)
