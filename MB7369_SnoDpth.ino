1ca2768f7ce5fedb3ac7596db4e5f8b5654f30de
Gleason <Hunter.Gleason@gov.bc.ca>
2022-09-29 11:41:06 -0700

    Fixed issue with depth calculation, iridium function now drops ultrasoinic readings with no fix (ie 5000 mm), tested non-iridum part ... working

--- MB7369_SnoDpth.ino
+++ MB7369_SnoDpth.ino
@@ -31,10 +31,11 @@ int err; //IRIDIUM status var
 int16_t *N; //Number of ultrasonic reange sensor readings to average.
 int sample_n; //same as N[0]
 int16_t *ultrasonic_height_mm;//Height of ultrasonic sensor,used to compute depth, ignored if stage_mm being logged
+int16_t height_mm;//Same as ultrasonic_height_mm[0]
 char **metrics_letter_code;// Three letter code for Iridium string, e.g., 'A' for stage 'E' for snow depth (see metrics_lookup in database)
 String metrics; //String for representing dist_letter_code
-int16_t distance; //Variable for holding distance read from MaxBotix MB7369 ultrasonic ranger
-int16_t duration; //Variable for holding pulse width duration read from MaxBotix ultrasonic ranger
+int32_t distance; //Variable for holding distance read from MaxBotix MB7369 ultrasonic ranger
+int32_t duration; //Variable for holding pulse width duration read from MaxBotix ultrasonic ranger
 sensors_event_t rh_prct, temp_deg_c; //Variable for holding AHT20 temp and RH vals
 
 
@@ -47,7 +48,7 @@ sensors_event_t rh_prct, temp_deg_c; //Variable for holding AHT20 temp and RH va
 RTC_DS3231 rtc; // Setup a PCF8523 Real Time Clock instance
 File dataFile; // Setup a log file instance
 IridiumSBD modem(IridiumSerial); // Declare the IridiumSBD object
-Adafruit_AHTX0 aht;//instantiate AHT20 object 
+Adafruit_AHTX0 aht;//instantiate AHT20 object
 QuickStats stats; //initialize an instance of QuickStats class
 
 
@@ -99,11 +100,11 @@ int send_hourly_data()//Function reads HOURLY.CSV and sends hourly averages over
   int num_rows = cp.getRowsCount();//Get # of rows
 
   datetimes = (char**)cp["datetime"];//populate datetimes
-  if (metrics.charAt(0) == 'E')//If measuring snow depth 
+  if (metrics.charAt(0) == 'E')//If measuring snow depth
   {
     distances = (int16_t*)cp["snow_depth_mm"];//populate snow depths
   } else {//If measuring stage
-    distances = (int16_t*)cp["stage_mm"];//populate stage 
+    distances = (int16_t*)cp["stage_mm"];//populate stage
   }
   air_temps = (float*)cp["air_temp_deg_c"];//populate air temps
   rhs = (float*)cp["rh_prct"];//populate RHs
@@ -126,11 +127,12 @@ int send_hourly_data()//Function reads HOURLY.CSV and sends hourly averages over
   while (start_dt < end_dt)//while the start datetime is < end datetime
   {
     intvl_dt = start_dt + TimeSpan(0, 1, 0, 0);//intvl_dt is equal to start_dt + 1 hour
-    
-    float mean_depth=-9999.0;//mean depth / distance
+
+    float mean_depth = -9999.0; //mean depth / distance
     float mean_temp;//mean air temp
     float mean_rh;//mean air RH
-    boolean is_first_obs = true;//Boolean indicating first observation 
+    boolean is_first_obs = true;//Boolean indicating first hourly observation
+    boolean is_first_obs_snow = true;//Boolean for first houry snow observation
     int N = 0;//Sample N counter
     int N_snow = 0;
 
@@ -146,53 +148,61 @@ int send_hourly_data()//Function reads HOURLY.CSV and sends hourly averages over
 
       DateTime obs_dt = DateTime(dt_year, dt_month, dt_day, dt_hour, dt_min, dt_sec);//Construct DateTime for obs at row i
 
-      if (obs_dt >= start_dt && obs_dt <= intvl_dt)//If obs datetime is withing the current hour interval 
+      if (obs_dt >= start_dt && obs_dt <= intvl_dt)//If obs datetime is withing the current hour interval
       {
 
         float snow_depth = (float) distances[i];//depth / distance at row i
         float air_temp = air_temps[i];//air temp at row i
         float rh = rhs[i];//RH at row i
 
-        if (is_first_obs == true)//Check if this is the first observation for the hour
+        //Need to treat snow diffrent as a good reading is not guarteed
+        if (is_first_obs_snow == true)
         {
-          if(snow_depth>-100.0)
+          //Check that depth is greater than zero (ie not negative) with 100mm of tolerance 
+          if (snow_depth > -100.0)
           {
             mean_depth = snow_depth;//mean depth / distance equal to depth at i
             N_snow++;
           }
-          mean_temp = air_temp;//mean_air temp equal to air temp at i
-          mean_rh = rh;//mean RH equal to RH at i 
-          is_first_obs = false;//No longer first obs
-          N++;//Increment sample N
         } else {
-          if(snow_depth>-100.0)
+          if (snow_depth > -100.0)
           {
             mean_depth = mean_depth + snow_depth;//Add depth / distance at i to mean_depth
             N_snow++;
           }
+        }
+
+        if (is_first_obs == true)//Check if this is the first observation for the hour
+        {
+
+          mean_temp = air_temp;//mean_air temp equal to air temp at i
+          mean_rh = rh;//mean RH equal to RH at i
+          is_first_obs = false;//No longer first obs
+          N++;//Increment sample N
+        } else {
           mean_temp = mean_temp + air_temp;//Add air temp at i to mean temp
           mean_rh = mean_rh + rh;//Add RH at i to mean RH
-          N++;//Increment sample N 
+          N++;//Increment sample N
         }
 
       }
     }
 
-    if(N_snow>0)
+    if (N_snow > 0)
     {
-      mean_depth = mean_depth / N;//mean depth / distance 
+      mean_depth = mean_depth / N;//mean depth / distance
     }
-    
+
     if (N > 0)//Check if there were any observations for the hour
     {
       mean_temp = (mean_temp / N) * 10.0;//mean air temp, convert to int
-      mean_rh = (mean_rh / N) *10.0;//mean RH, convert to int 
-      
+      mean_rh = (mean_rh / N) * 10.0; //mean RH, convert to int
+
       datastring = datastring + String(round(mean_depth)) + ',' + String(round(mean_temp)) + ',' + String(round(mean_rh)) + ':';//Assemble the data string
 
     }
 
-    start_dt = intvl_dt;//Set intvl_dt to start_dt,i.e., next hour 
+    start_dt = intvl_dt;//Set intvl_dt to start_dt,i.e., next hour
   }
 
   uint8_t dt_buffer[340];//Binary bufffer for iridium transmission (max allowed buffer size 340 bytes)
@@ -204,7 +214,7 @@ int send_hourly_data()//Function reads HOURLY.CSV and sends hourly averages over
   for (int i = 0; i < message_bytes; i++)//For each byte in the message (i.e. each char)
   {
     dt_buffer[buffer_idx] = datastring.charAt(i);//Update the buffer at buffer index with corresponding char
-    buffer_idx++;//increment buffer index 
+    buffer_idx++;//increment buffer index
   }
 
   modem.setPowerProfile(IridiumSBD::USB_POWER_PROFILE);// Prevent from trying to charge to quickly, low current setup
@@ -215,7 +225,7 @@ int send_hourly_data()//Function reads HOURLY.CSV and sends hourly averages over
   {
     modem.begin();
   }
-  
+
   digitalWrite(led, HIGH); //Indicate the modem is trying to send with led
 
   err = modem.sendSBDBinary(dt_buffer, buffer_idx); //transmit binary buffer data via iridium
@@ -231,9 +241,9 @@ int send_hourly_data()//Function reads HOURLY.CSV and sends hourly averages over
 
   digitalWrite(irid_pwr_pin, LOW);//Kill power to modem to save pwr
 
-  SD.remove("/HOURLY.CSV");  //Remove previous daily values CSV 
+  SD.remove("/HOURLY.CSV");  //Remove previous daily values CSV
 
-  return err;  //Return err code, not used but can be used for trouble shooting 
+  return err;  //Return err code, not used but can be used for trouble shooting
 
 }
 
@@ -245,13 +255,13 @@ void setup(void)//Setup section, runs once upon powering up the Feather M0
   digitalWrite(PeriSetPin, LOW);//Drive peripheral set pin LOW
   pinMode(PeriUnsetPin, OUTPUT);//Set peripheral unset pin as OUTPUT
   digitalWrite(PeriUnsetPin, HIGH);//Drive peripheral unset pin HIGH to assure unset relay
-  delay(30);//Mimimum delay for relay 
+  delay(30);//Mimimum delay for relay
   digitalWrite(PeriUnsetPin, LOW);//Drive peripheral unset pin LOW as it is a latching relay
   pinMode(irid_pwr_pin, OUTPUT);//Set iridium power pin as OUTPUT
   digitalWrite(irid_pwr_pin, LOW);//Drive iridium power pin LOW
   pinMode(triggerPin, OUTPUT);//Set ultrasonic ranging trigger pin as OUTPUT
   digitalWrite(triggerPin, LOW);//Drive ultrasonic randing trigger pin LOW
-  pinMode(pulsePin, INPUT);//Set ultrasonic pulse width pin as INPUT 
+  pinMode(pulsePin, INPUT);//Set ultrasonic pulse width pin as INPUT
 
   while (!SD.begin(chipSelect)) {//Make sure a SD is available (2-sec flash led means SD card did not initialize)
     digitalWrite(led, HIGH);
@@ -270,16 +280,16 @@ void setup(void)//Setup section, runs once upon powering up the Feather M0
     delay(1000);
   }
 
-  filename = (char**)cp["filename"];//Get file name from parameter file 
+  filename = (char**)cp["filename"];//Get file name from parameter file
   sample_intvl = (int16_t*)cp["sample_intvl"];//Get sample interval in seconds from parameter file
   irid_freq = (int16_t*)cp["irid_freq"];//Get iridium freqency in hours from parameter file
   start_time = (char**)cp["start_time"];//Get idridium start time as timestamp from parameter file
-  N = (int16_t*)cp["N"];//Get sample N from parameter file 
-  sample_n = N[0];//Update value of sample_n from parameter file 
+  N = (int16_t*)cp["N"];//Get sample N from parameter file
+  sample_n = N[0];//Update value of sample_n from parameter file
   ultrasonic_height_mm = (int16_t*)cp["ultrasonic_height_mm"];//Get height of ultrasonic sensor in mm from parameter file
-  metrics_letter_code = (char**)cp["metrics_letter_code"];//Get metrics letter code string from parameter file 
+  metrics_letter_code = (char**)cp["metrics_letter_code"];//Get metrics letter code string from parameter file
 
-  metrics = String(metrics_letter_code[0]);//Update metrics with value from parameter file 
+  metrics = String(metrics_letter_code[0]);//Update metrics with value from parameter file
 
   sleep_time = sample_intvl[0] * 1000;//Sleep time between samples in miliseconds
 
@@ -287,9 +297,12 @@ void setup(void)//Setup section, runs once upon powering up the Feather M0
 
   irid_freq_hrs = irid_freq[0];  //Iridium transmission frequency in hours
 
+  height_mm = ultrasonic_height_mm[0];
+
+
   int start_hour = String(start_time[0]).substring(0, 3).toInt();//Parse iridium start time hour
   int start_minute = String(start_time[0]).substring(3, 5).toInt();//Parse iridium start time minute
-  int start_second = String(start_time[0]).substring(6, 8).toInt();//Parse iridium start time second 
+  int start_second = String(start_time[0]).substring(6, 8).toInt();//Parse iridium start time second
 
   while (!rtc.begin())// Make sure RTC is available, blink at 1/2 sec if not
   {
@@ -301,7 +314,7 @@ void setup(void)//Setup section, runs once upon powering up the Feather M0
 
   present_time = rtc.now();  //Get the present datetime
 
-//Update the transmit time to the start time for present date
+  //Update the transmit time to the start time for present date
   transmit_time = DateTime(present_time.year(),
                            present_time.month(),
                            present_time.day(),
@@ -328,17 +341,18 @@ void loop(void)//Code executes repeatedly until loss of power
   digitalWrite(PeriSetPin, HIGH);//Drive set pin high
   delay(50);//Required delay
   digitalWrite(PeriSetPin, LOW);//Drive set pin LOW (latched)
-  delay(200);//Let settle  
-  
+  delay(200);//Let settle
+
   distance = read_sensor(sample_n);//Read N average ranging distance from MaxBotix ultrasonic ranger
+
 
   if (metrics.charAt(0) == 'E')//If the first letter of the metrics letter code is 'E', i.e., snow_depth_mm, otherwise its 'A', i.e., stage_mm
   {
-    distance = ultrasonic_height_mm[0] - distance;//Compute snow depth from distance 
+    distance = height_mm - distance;//Compute snow depth from distance
   }
 
 
- if (!aht.begin()) { // Start AHT20 (2-sec flash led means SHT30 did not initialize)
+  if (!aht.begin()) { // Start AHT20 (2-sec flash led means SHT30 did not initialize)
     digitalWrite(led, HIGH);
     delay(5000);
     digitalWrite(led, LOW);
@@ -352,21 +366,21 @@ void loop(void)//Code executes repeatedly until loss of power
   delay(50);//required delay
   digitalWrite(PeriUnsetPin, LOW);//Drive unset pin LOW (latched)
 
-  String datastring = present_time.timestamp() + "," + distance + "," + temp_deg_c.temperature + "," + rh_prct.relative_humidity;//Assemble datastring 
+  String datastring = present_time.timestamp() + "," + distance + "," + temp_deg_c.temperature + "," + rh_prct.relative_humidity;//Assemble datastring
 
   if (!SD.exists(filestr.c_str()))//Write header if first time writing to the logfile
   {
-    dataFile = SD.open(filestr.c_str(), FILE_WRITE);//Open file under filestr name from parameter file 
+    dataFile = SD.open(filestr.c_str(), FILE_WRITE);//Open file under filestr name from parameter file
     if (dataFile)
     {
-      if (metrics.charAt(0) == 'E')//If first letter of metrics code is 'E', i.e., snow_depth_mm, write snow header 
+      if (metrics.charAt(0) == 'E')//If first letter of metrics code is 'E', i.e., snow_depth_mm, write snow header
       {
         dataFile.println("datetime,snow_depth_mm,air_temp_deg_c,rh_prct");
       } else {//stage_mm, 'A'
-        dataFile.println("datetime,stage_mm,air_temp_deg_c,rh_prct");//Write stage header 
+        dataFile.println("datetime,stage_mm,air_temp_deg_c,rh_prct");//Write stage header
       }
 
-      dataFile.close();//Close the dataFile 
+      dataFile.close();//Close the dataFile
     }
 
   } else {
@@ -379,8 +393,8 @@ void loop(void)//Code executes repeatedly until loss of power
     }
 
   }
-  
-  if (!SD.exists("HOURLY.CSV"))//HOURLY.CSV duplicates the log file until deleted 
+
+  if (!SD.exists("HOURLY.CSV"))//HOURLY.CSV duplicates the log file until deleted
   {
     dataFile = SD.open("HOURLY.CSV", FILE_WRITE);
     if (dataFile)
